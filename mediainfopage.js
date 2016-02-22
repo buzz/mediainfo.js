6badc37d0fe2a7b95ab618bcc687fd846297d113
buzz <buzz@users.noreply.github.com>
2016-02-22 19:19:54 +0100

    drag n drop

--- mediainfopage.js
+++ mediainfopage.js
@@ -1,57 +1,93 @@
-// Parse file chunk by chunk
-function parseFile(mi, file, cb) {
-  var CHUNK_SIZE = 20 * 1024 * 1024; // 1MiB
-  var offset = 0, state;
-  var reader = new FileReader();
+$(function() {
+  var $dropzone = $('#dropzone, #dropzone *');
+  var $fileInput = $('#fileinput');
+  var $result = $('#result');
+  var $status = $('#status');
 
-  console.debug('Analyzing file: ', file);
+  var CHUNK_SIZE = 5 * 1024 * 1024;
+  var miLib, mi, offset, state;
 
-  mi.open_buffer_init(file.size, offset);
+  // Parse file chunk by chunk
+  function parseFile(file, cb) {
+    var reader = new FileReader();
+    offset = 0;
 
-  reader.onload = function() {
-    var arr = new Uint8Array(reader.result);
-    state = mi.open_buffer_continue(arr, arr.length);
+    console.debug('Analyzing file: ', file.name + ' (' + file.size + ')');
 
-    if ((state >> 3) % 2 !== 0 || offset >= file.size) {
-      var result = mi.inform();
-      mi.close();
-      cb(result);
-      return;
+    mi.open_buffer_init(file.size, offset);
+
+    reader.onload = function() {
+      var arr = new Uint8Array(reader.result);
+      state = mi.open_buffer_continue(arr, arr.length);
+      arr = null;
+
+      if ((state >> 3) % 2 !== 0 || offset >= file.size) {
+        var result = mi.inform();
+        mi.close();
+        cb(result);
+        return;
+      }
+
+      $status.text('Read ' + offset + ' bytes (' + (offset / file.size * 100).toFixed(1) + '% of file)', state);
+
+      offset += CHUNK_SIZE;
+      seek();
+    };
+
+    reader.onerror = function(e) {
+      throw('Error while reading file', e);
+    };
+
+    function seek() {
+      var slice = file.slice(offset, offset + CHUNK_SIZE);
+      reader.readAsArrayBuffer(slice);
     }
-
-    console.debug('Read ' + offset + ' bytes (' + (offset / file.size * 100).toFixed(1) + '% of file)');
-
-    offset += CHUNK_SIZE;
     seek();
   };
 
-  reader.onerror = function(e) {
-    throw('Error while reading file', e);
-  };
-
-  function seek() {
-    var slice = file.slice(offset, offset + CHUNK_SIZE);
-    reader.readAsArrayBuffer(slice);
+  // show result
+  function showResult(r) {
+    $result.text(r);
   }
-  seek();
-};
 
-function MediaInfoStart() {
-  console.debug('mediainfo.js initialized');
-
-  var mi = new Module.MediaInfo();
-
-  var $dropzone = $('#dropzone');
-  var $fileInput = $('#fileinput');
-  var $result = $('#result');
-
-  $fileInput.on('change', function(e) {
-    var el = $fileInput.get(0);
-    if (el.files.length > 0) {
-      var file = el.files[0];
-      parseFile(mi, file, function(r) {
-        $result.text(r);
-      });
+  // init drag 'n drop
+  $dropzone.on('dragover', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  })
+  $dropzone.on('dragleave', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $dropzone.removeClass('dragover');
+  })
+  $dropzone.on('dragenter', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $dropzone.addClass('dragover');
+  })
+  $dropzone.on('drop', function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    $dropzone.removeClass('dragover');
+    if(e.originalEvent.dataTransfer){
+      if(e.originalEvent.dataTransfer.files.length > 0) {
+        parseFile(e.originalEvent.dataTransfer.files[0], showResult);
+      }
     }
   });
-}
+
+  // init mediainfo
+  miLib = MediaInfo(function() {
+    console.debug('mediainfo.js initialized');
+
+    window['miLib'] = miLib;    // debug
+    mi = new miLib.MediaInfo();
+
+    $fileInput.on('change', function(e) {
+      var el = $fileInput.get(0);
+      if (el.files.length > 0) {
+        parseFile(el.files[0], showResult);
+      }
+    });
+  });
+});
