49bd72e36224f92efa1412eacdc798f230fb839b
Jeremiah Hoyet <jerhoyet@gmail.com>
2019-08-05 13:49:01 -0400

    Update dependencies to latest versions: * Update libmediainfo to 19.07 * Update libzen to 0.4.37 * Update build to output asm.js compatible code instead of WASM (the default)

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # mediainfo.js
 OPTS="-O2"
 em++ ${OPTS} -DUNICODE -std=c++11 -I MediaInfoLib/Source -I ZenLib/Source --bind -c mediainfojs.cpp && \
-em++ ${OPTS} --llvm-lto 0  -s TOTAL_MEMORY=1073741824 -s NO_FILESYSTEM=1 -s MODULARIZE=1 --bind mediainfojs.o MediaInfoLib/Project/GNU/Library/.libs/libmediainfo.a ZenLib/Project/GNU/Library/.libs/libzen.a Shared/Source/zlib/libz.a -o mediainfo.js
+em++ ${OPTS} --llvm-lto 0  -s TOTAL_MEMORY=1073741824 -s LEGACY_VM_SUPPORT=1 -s WASM=0 -s NO_FILESYSTEM=1 -s MODULARIZE=1 --bind mediainfojs.o MediaInfoLib/Project/GNU/Library/.libs/libmediainfo.a ZenLib/Project/GNU/Library/.libs/libzen.a Shared/Source/zlib/libz.a -o mediainfo.js
 mv mediainfo.js mediainfo_.js
 cat pre.js mediainfo_.js post.js > mediainfo.js
 rm mediainfo_.js
