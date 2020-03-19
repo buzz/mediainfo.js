bc178029b4699e574f7caaf91d1a3f78938a9931
buzz <buzz@users.noreply.github.com>
2020-03-19 03:39:41 +0100

    Create minified build using terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,25 @@
 import path from 'path'
-import commonjs from '@rollup/plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+
+const name = 'MediaInfo'
+const format = 'umd'
 
 export default {
   input: path.resolve(__dirname, 'src', 'index.js'),
-  output: {
-    file: path.resolve(__dirname, 'dist', 'mediainfo.js'),
-    format: 'umd',
-    name: 'MediaInfo'
-  },
-  plugins: [commonjs()]
-};
+  output: [
+    {
+      file: path.resolve(__dirname, 'dist', 'mediainfo.js'),
+      format,
+      name,
+    },
+    {
+      file: path.resolve(__dirname, 'dist', 'mediainfo.min.js'),
+      format,
+      name,
+      plugins: [terser()],
+    },
+  ],
+  external: ['fs', 'path'],
+  plugins: [commonjs()],
+}
