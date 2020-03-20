14f796905d8165b9f94c9c4051f895d823a7aa9f
buzz <buzz@users.noreply.github.com>
2020-03-21 00:26:26 +0100

    Copy CNAME to /dist

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,10 @@ module.exports = {
         from: resolve('node_modules', 'mediainfo.js', 'dist', 'mediainfo.wasm'),
         to: '.',
       },
+      {
+        from: 'CNAME',
+        to: '.',
+      },
     ]),
     new HtmlWebpackRootPlugin(),
     ...(devMode
