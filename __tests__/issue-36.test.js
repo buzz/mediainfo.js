1ef67e8b7ccc361c31b2749844e56bec08446d20
buzz <buzz@users.noreply.github.com>
2020-07-28 15:23:31 +0200

    Add tests for issue #36

---
+++ __tests__/issue-36.test.js
@@ -0,0 +1,60 @@
+// Test problematic MPG file
+// https://github.com/buzz/mediainfo.js/issues/36
+import path from 'path'
+
+import MediaInfo from '../dist/mediainfo'
+import analyzeFile from './analyzeFile'
+
+const FILEPATH = path.resolve(__dirname, 'fixtures', 'centaur_1.mpg')
+let mi
+
+beforeEach(async () => {
+  mi = await MediaInfo()
+})
+
+afterEach(() => mi.close)
+
+describe('Issue #36: centaur_1.mpg', () => {
+  it('should parse file', async () => {
+    expect.assertions(33)
+
+    const result = await analyzeFile(mi, FILEPATH)
+    expect(result).toBeInstanceOf(Object)
+    const { track } = result.media
+    expect(track).toHaveLength(2)
+    const [track0, track1] = track
+
+    expect(track0['@type']).toBe('General')
+    expect(track0.Format).toBe('MPEG Video')
+    expect(track0.Format_Version).toBe('1')
+    expect(track0.FileSize).toBe('563016')
+    expect(track0.Duration).toBe('13.981')
+    expect(track0.OverallBitRate_Mode).toBe('CBR')
+    expect(track0.OverallBitRate).toBe('322161')
+
+    expect(track1['@type']).toBe('Video')
+    expect(track1.Format).toBe('MPEG Video')
+    expect(track1.Format_Version).toBe('1')
+    expect(track1.Format_Settings_BVOP).toBe('Yes')
+    expect(track1.Format_Settings_Matrix).toBe('Default')
+    expect(track1.Format_Settings_GOP).toBe('M=3, N=15')
+    expect(track1.Duration).toBe('13.981')
+    expect(track1.BitRate_Mode).toBe('CBR')
+    expect(track1.BitRate).toBe('320000')
+    expect(track1.Width).toBe('320')
+    expect(track1.Height).toBe('240')
+    expect(track1.DisplayAspectRatio).toBe('1.333')
+    expect(track1.FrameRate).toBe('29.970')
+    expect(track1.FrameCount).toBe('419')
+    expect(track1.ColorSpace).toBe('YUV')
+    expect(track1.ChromaSubsampling).toBe('4:2:0')
+    expect(track1.BitDepth).toBe('8')
+    expect(track1.ScanType).toBe('Progressive')
+    expect(track1.Compression_Mode).toBe('Lossy')
+    expect(track1.Delay).toBe('0.000')
+    expect(track1.TimeCode_FirstFrame).toBe('00:00:00:00')
+    expect(track1.Gop_OpenClosed).toBe('Closed')
+    expect(track1.StreamSize).toBe('563016')
+    expect(track1.BufferSize).toBe('40960')
+  })
+})
