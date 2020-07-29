91dd6173a0ef17fd72bfb767c34983e2e3db7345
buzz <buzz@users.noreply.github.com>
2020-07-29 17:47:15 +0200

    Add test for cover data

--- __tests__/download-fixtures.sh
+++ __tests__/download-fixtures.sh
@@ -4,6 +4,7 @@ set -e
 
 urls=(
   "a51c3aff106210abcf32a9d4285628a6,https://github.com/mathiasbynens/small/raw/master/AudioVideoInterleave.avi"
+  "b02fc030703403a13c9a6cef5922c6d1,https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Creative_Commons/Dead_Combo/CC_Affiliates_Mixtape_1/Dead_Combo_-_01_-_Povo_Que_Cas_Descalo.mp3"
 )
 
 mkdir -p ./fixtures
