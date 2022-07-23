3956d52d0637471348deb4a39b1449b6f72cbd37
buzz <buzz@users.noreply.github.com>
2022-07-23 15:23:00 +0200

    Fix CLI

--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 import { promises as fsPromises } from 'fs'
-import yargs = require('yargs')
+import * as yargs from 'yargs'
+import { Argv } from 'yargs'
 
 import MediaInfoFactory from '../dist/mediainfo'
-import type { FormatType, MediaInfo, ReadChunkFunc } from './types'
+import type { FormatType, MediaInfo, ReadChunkFunc, Result } from './types'
 
 interface Arguments {
   file: string
@@ -14,10 +15,41 @@ interface Arguments {
 
 const formatChoices: ReadonlyArray<FormatType> = ['JSON', 'XML', 'HTML', 'text']
 
-const analyze = async ({ file, format, coverData }: Arguments) => {
+const parser = yargs
+  .option('format', {
+    alias: 'f',
+    default: 'text',
+    describe: 'Choose format',
+    choices: formatChoices,
+    type: 'string',
+  })
+  .option('cover-data', {
+    default: false,
+    describe: 'Output cover data as base64',
+    type: 'boolean',
+  })
+  .command('$0 <file>', 'Show information about media files', (yargs: Argv) => {
+    yargs.positional('file', {
+      describe: 'File to analyze',
+      type: 'string',
+    })
+  })
+  .fail(function (msg: string, err: Error, yargs) {
+    if (msg) {
+      console.error(yargs.help())
+      console.error(msg)
+    }
+    if (err) {
+      console.error(err.message)
+    }
+    process.exit(1)
+  })
+
+const analyze = async ({ coverData, file, format }: Arguments) => {
   let fileHandle: fsPromises.FileHandle | undefined
   let fileSize: number
   let mediainfo: MediaInfo | undefined
+  let result: Result
 
   if (coverData && !['JSON', 'XML'].includes(format)) {
     throw TypeError('For cover data you need to choose JSON or XML as output format!')
@@ -33,49 +65,26 @@ const analyze = async ({ file, format, coverData }: Arguments) => {
     fileHandle = await fsPromises.open(file, 'r')
     fileSize = (await fileHandle.stat()).size
     mediainfo = (await MediaInfoFactory({ format, coverData })) as MediaInfo
-    const result = await mediainfo.analyzeData(() => fileSize, readChunk)
-    console.log(result)
-  } catch (err) {
-    throw err
+    result = (await mediainfo.analyzeData(() => fileSize, readChunk)) as Result
   } finally {
     fileHandle && (await fileHandle.close())
     mediainfo && mediainfo.close()
   }
+
+  return result
 }
 
-yargs
-  .option('format', {
-    alias: 'f',
-    default: 'text',
-    describe: 'Choose format',
-    choices: formatChoices,
-    type: 'string',
-  })
-  .option('cover-data', {
-    default: false,
-    describe: 'Output cover data as base64',
-    type: 'boolean',
-  })
-  .command(
-    '$0 <file>',
-    'Show information about media files',
-    (yargs) => {
-      yargs.positional('file', {
-        describe: 'File to analyze',
-        type: 'string',
-      })
-    },
-    analyze
-  )
-  .fail(function (msg, err, yargs) {
-    if (msg) {
-      console.error(yargs.help())
-      console.error(msg)
-    }
-    if (err) {
-      console.error(err.message)
-    }
-    process.exit(1)
-  })
-  .help()
-  .parse()
+;(async () => {
+  const argv = await parser.argv
+
+  try {
+    const result = await analyze({
+      coverData: argv.coverData,
+      file: argv.file as string,
+      format: argv.format as FormatType,
+    })
+    console.log(result)
+  } catch (err) {
+    console.error(err)
+  }
+})()
