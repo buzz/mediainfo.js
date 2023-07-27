9ebae1fb09ec75dfe52c577b40531289a5a763a0
buzz <buzz@users.noreply.github.com>
2023-07-27 14:30:41 +0200

    chore(types): export more types from package
    Export all types that may be relevant to users when using this library.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import type MediaInfo from './MediaInfo'
-import type { GetSizeFunc, ReadChunkFunc, ResultMap } from './MediaInfo'
+import type { FormatType, GetSizeFunc, ReadChunkFunc, ResultMap } from './MediaInfo'
 import MediaInfoFactory, { type MediaInfoFactoryOptions } from './MediaInfoFactory'
+import type { CreationType, ExtraType, TrackType, MediaType, MediaInfoType } from './MediaInfoType'
 
-export type { GetSizeFunc, MediaInfo, MediaInfoFactoryOptions, ReadChunkFunc, ResultMap }
+export type {
+  FormatType,
+  GetSizeFunc,
+  MediaInfo,
+  MediaInfoFactoryOptions,
+  ReadChunkFunc,
+  ResultMap,
+  CreationType,
+  ExtraType,
+  TrackType,
+  MediaType,
+  MediaInfoType,
+}
 export default MediaInfoFactory
