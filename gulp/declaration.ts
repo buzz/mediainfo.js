808419f1990bbb3773caae3f5a59af1e41ee54c3
buzz <buzz@users.noreply.github.com>
2023-07-27 14:27:15 +0200

    chore(types): fix declaration task
    Copy custom .d.ts files to /dist.

--- gulp/declaration.ts
+++ gulp/declaration.ts
@@ -1,12 +1,31 @@
-import { PROJECT_DIR } from './constants'
+import { join } from 'path'
+
+import gulp from 'gulp'
+
+import { DIST_DIR, PROJECT_DIR, SRC_DIR } from './constants'
 import { spawn } from './utils'
 
-function declaration() {
+function generateDeclaration() {
   return spawn(
     'tsc',
-    ['--emitDeclarationOnly', '--declarationDir', 'dist', '--declaration', 'true', 'src/index.ts'],
+    [
+      '--emitDeclarationOnly',
+      '--declarationDir',
+      DIST_DIR,
+      '--declaration',
+      'true',
+      join(SRC_DIR, 'index.ts'),
+    ],
     PROJECT_DIR
   )
 }
 
-export default declaration
+function copyDeclaration() {
+  return gulp.src(join(SRC_DIR, 'MediaInfoModule.d.ts')).pipe(gulp.dest(DIST_DIR))
+}
+
+const declarationTask = gulp.parallel([generateDeclaration, copyDeclaration])
+declarationTask.displayName = 'declaration'
+declarationTask.description = 'Generate TS declaration'
+
+export default declarationTask
