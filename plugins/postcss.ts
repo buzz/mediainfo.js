73ab9bd9135c19e82a22677b112edf3267f93fca
buzz <buzz@users.noreply.github.com>
2024-06-01 00:37:23 +0200

    Upgrade packages

--- plugins/postcss.ts
+++ plugins/postcss.ts
@@ -1,7 +1,7 @@
 // @ts-expect-error No declarations available
 import postcssColorModFunction from 'postcss-color-mod-function'
 // @ts-expect-error No declarations available
-import postcssEach from 'postcss-each'
+import postcssEach from 'postcss-each' // pinned to 0.10.0 for compatibility with postcss-color-mod-function
 import type { PluginModule } from '@docusaurus/types'
 
 const postcss: PluginModule = () => ({
