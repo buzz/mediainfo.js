f3e5e2771cb9541535e53c28e91b7b7c2f69b72d
buzz <buzz@users.noreply.github.com>
2025-08-30 18:31:53 +0200

    test: simplify `error.wasmLoading.test.ts` by removing uncaught exception handling

--- __tests__/error.wasmLoading.test.ts
+++ __tests__/error.wasmLoading.test.ts
@@ -1,20 +1,7 @@
 import mediaInfoFactory from '..'
 import { expectToBeError } from './utils'
 
-const originalProcess = process._original()
-const originalUncaughtExceptionListeners: NodeJS.UncaughtExceptionListener[] = []
-const originalUnhandledRejectionListeners: NodeJS.UnhandledRejectionListener[] = []
-
 beforeEach(() => {
-  for (const listener of originalProcess.listeners('uncaughtException')) {
-    originalUncaughtExceptionListeners.push(listener)
-    originalProcess.off('uncaughtException', listener)
-  }
-  for (const listener of originalProcess.listeners('unhandledRejection')) {
-    originalUnhandledRejectionListeners.push(listener)
-    originalProcess.off('unhandledRejection', listener)
-  }
-
   // Suppress console output from emscripten module
   jest.spyOn(console, 'error')
   // @ts-expect-error TS doesn't know mockImplementation
@@ -24,63 +11,32 @@ beforeEach(() => {
 
 afterEach(() => {
   jest.restoreAllMocks()
-
-  for (const listener of originalUncaughtExceptionListeners) {
-    originalProcess.on('uncaughtException', listener)
-  }
-  for (const listener of originalUnhandledRejectionListeners) {
-    originalProcess.on('unhandledRejection', listener)
-  }
 })
 
 describe('Error on WASM loading', () => {
   it('should return error via callback and throw Exception', (done) => {
-    let asyncCatched = false
-
-    // Listener for uncaught exceptions
-    const errorListener = (err: Error) => {
-      expect(err.message).toMatch('no such file')
-      expect(asyncCatched).toBeTruthy()
-      originalProcess.removeListener('uncaughtException', errorListener)
-      done()
-    }
-
-    originalProcess.on('uncaughtException', errorListener)
-
     mediaInfoFactory(
       { locateFile: () => 'file_does_not_exist.wasm' },
       () => {
-        done.fail('Resolve callback should not fire')
+        done('Resolve callback should not fire')
       },
       (error) => {
         expectToBeError(error)
         expect(error.message).toMatch('no such file')
-        asyncCatched = true
+        done()
       }
     )
   })
 
   it('should return error via Promise and throw Exception', (done) => {
-    let asyncCatched = false
-
-    // Listener for uncaught exceptions
-    const errorListener = (err: Error) => {
-      expect(err.message).toMatch('no such file')
-      expect(asyncCatched).toBeTruthy()
-      originalProcess.removeListener('uncaughtException', errorListener)
-      done()
-    }
-
-    originalProcess.on('uncaughtException', errorListener)
-
     mediaInfoFactory({ locateFile: () => 'file_does_not_exist.wasm' })
       .then(() => {
-        done.fail('Resolve callback should not fire')
+        done('Resolve callback should not fire')
       })
       .catch((error: unknown) => {
         expectToBeError(error)
         expect(error.message).toMatch('no such file')
-        asyncCatched = true
+        done()
       })
   })
 })
--- env.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace NodeJS {
-  export interface Process {
-    _original: () => Process
-  }
-}
